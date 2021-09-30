68345b106d1e147f8c02e741461090b2aa8db1b1
Kimmo Saari <kimmo.saari@aava.fi>
2021-09-30 16:12:29 +0300

    Add `rsync-snapshot-to-remote` addon

--- rsync-snapshot-to-remote/run.sh
+++ rsync-snapshot-to-remote/run.sh
@@ -9,15 +9,35 @@ RSYNC_USER=$(jq --raw-output ".rsync_user" $CONFIG_PATH)
 RSYNC_PASSWORD=$(jq --raw-output ".rsync_password" $CONFIG_PATH)
 REMOTE_DIRECTORY=$(jq --raw-output ".remote_directory" $CONFIG_PATH)
 
-echo "[INFO] Sync snapshots to remote started..."
+START=$(date +%s)
+res1=$(date +%s.%N)
+
+echo "$(date +"%Y-%m-%d-%H_%M_%S") - [INFO] Sync snapshots to remote started..."
 
 function copy-backup-to-remote {
     rsyncurl="$RSYNC_USER@$RSYNC_HOST::$REMOTE_DIRECTORY"
     echo "[INFO] Syncing /backup to ${REMOTE_DIRECTORY} on ${RSYNC_HOST} using rsync"
-    sshpass -p "${RSYNC_PASSWORD}" rsync -av /backup/ "${rsyncurl}" --ignore-existing --progress
+    ls -latrh
+    sshpass -p "${RSYNC_PASSWORD}" rsync -av /backup/ "${rsyncurl}" --ignore-existing
 }
 
 copy-backup-to-remote
 
-echo "[INFO] Sync snapshots to remote Completed!"
+STOP=$(date +%s)
+
+END=$((STOP - START))
+
+echo "$(date +"%Y-%m-%d-%H_%M_%S") - [INFO] Sync snapshots to remote Completed!"
+echo "$(date +"%Y-%m-%d-%H_%M_%S") - [INFO] Synced in ${END} seconds."
+
+res2=$(date +%s.%N)
+dt=$(echo "$res2 - $res1" | bc)
+dd=$(echo "$dt/86400" | bc)
+dt2=$(echo "$dt-86400*$dd" | bc)
+dh=$(echo "$dt2/3600" | bc)
+dt3=$(echo "$dt2-3600*$dh" | bc)
+dm=$(echo "$dt3/60" | bc)
+ds=$(echo "$dt3-60*$dm" | bc)
+
+LC_NUMERIC=C printf "Total runtime: %d:%02d:%02d:%02.4f\n" "${dd}" "${dh}" "${dm}" "${ds}"
 exit 0
