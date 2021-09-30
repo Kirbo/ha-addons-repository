85732e720cc1a2b75faa7b1c965c9bf80f1f47d2
Kimmo Saari <kimmo.saari@aava.fi>
2021-09-30 17:59:28 +0300

    Add `rsync-snapshot-to-remote` addon

--- rsync-snapshot-to-remote/run.sh
+++ rsync-snapshot-to-remote/run.sh
@@ -22,15 +22,16 @@ function copy-backup-to-remote {
     echo
     echo "[INFO] Local files:"
     ls -ltrh /backup
+    (cd /backup && du -sch)
     LOCAL_FILES=$(ls -trh /backup)
 
     echo "---"
     echo "[INFO] Remote files:"
     sshpass -e ssh -o StrictHostKeyChecking=no ${SSH_URL} "ls -ltrh ${REMOTE_DIRECTORY}"
-    REMOTE_FILES=$(sshpass -e ssh -o StrictHostKeyChecking=no ${SSH_URL} "ls -trh ${REMOTE_DIRECTORY}")
+    REMOTE_FILES=$(sshpass -e ssh -o StrictHostKeyChecking=no ${SSH_URL} "ls -trh ${REMOTE_DIRECTORY} && cd ${REMOTE_DIRECTORY} && du -sch")
     NEW_FILES=$(diff <(echo "${REMOTE_FILES}") <(echo "${LOCAL_FILES}") | grep -E "(>|\+)")
 
-    if [[ -z "${NEW_FILES}" ]]; then
+    if [ -n "${NEW_FILES}" ]; then
         echo "---"
         echo "[INFO] Everything already synced!"
     else
